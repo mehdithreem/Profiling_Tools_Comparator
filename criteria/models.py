fcc9adbda0601a948506d3d5e8e119bc87b9dce1
mehdithreem <mehdithreem@gmail.com>
2018-02-12 15:55:36 +0330

    - minor changes in criteria fields

--- criteria/models.py
+++ criteria/models.py
@@ -7,21 +7,27 @@ from tools.models import Tool
 class Criteria(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField(blank=True)
-    benefits = models.IntegerField(default=0)
-    hurts = models.IntegerField(default=0)
+    benefits = models.FloatField(default=0.0)
+    hurts = models.FloatField(default=0.0)
     parent = models.ForeignKey('Criteria', on_delete=models.CASCADE, null=True, blank=True)
 
-    category = models.CharField(max_length=150, null=True)
-
     def __str__(self):
-        return '[' + str(self.pk) + '] [' + str(self.category) + '] ' + self.name + ' -'
+        return '[' + str(self.pk) + '] ' + self.name + ' -'
 
 class CriteriaScore(models.Model):
     criteria = models.ForeignKey(Criteria, on_delete=models.CASCADE)
     tool = models.ForeignKey(Tool, on_delete=models.CASCADE)
 
-    score = models.IntegerField(default=0)
+    score = models.FloatField(default=0.0)
     notes = models.TextField(blank=True)
+
+    SCORE_MODE_CHOICES = (
+        ('0', 'From Children'),
+        ('1', 'No Children'),
+        ('2', 'Use Both'),
+    )
+
+    score_mode = models.CharField(max_length=1, choices=SCORE_MODE_CHOICES, default='0')
 
     class Meta:
         unique_together = (('criteria', 'tool'),)
